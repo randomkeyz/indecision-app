e09ae915e6f6958c86fea6d1f08d8e0895333703
Natasha Hanna <nhanna@envisiagroup.com>
2018-07-06 16:41:40 -0500

    Going over conditional rendering

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
+// babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+
 console.log('App.js is running!');
 
-// JSX - JavaScript XML
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
+};
+
 var template = (
 	<div>
-		<h1>Indecision App</h1>
-		<p>This is some info</p>
+		<h1>{app.title}</h1>
+                <p>{app.options.length > 0 ? 'Here are your options' : 'You have no options'}</p>
+                {app.subtitle && <p>{app.subtitle}</p>}
 		<ol>
 			<li></li>
 			<li></li>
@@ -12,15 +20,26 @@ var template = (
 	</div>
 );
 
+var user = {
+    name: 'Shuri',
+    age: 22,
+    location: 'Wakanda'
+};
+
+function getLocation(location){
+    if (location){
+        return <p>Location: {location}</p>;
+    } 
+}
 
 var templateTwo = (
 	<div>
-		<h1>Bruce Wayne</h1>
-		<p>Age: 123</p>
-		<p>Location: USA</p>
+		<h1>{user.name ? user.name : 'Anonymous'}</h1>
+                {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+                {getLocation(user.location)}
 	</div>
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
