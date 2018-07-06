d2f839f0d005d38c6220e7be0111e62ae2b184a2
Natasha <randomkeyz@gmail.com>
2018-07-06 07:03:01 -0400

    Initial setup

---
+++ src/app.js
@@ -0,0 +1,8 @@
+console.log('App.js is running!');
+
+// JSX - JavaScript XML
+var template = <p>Changing!</p>;
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
